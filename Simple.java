c5ab8d11f202801ece29d3bfb042cfdaae562f56
internifrs <internsifrs@protominds.com>
2019-05-24 15:26:35 +0530

    divide

--- Simple.java
+++ Simple.java
@@ -1,5 +1,9 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     int a=2;
+     int b=2;
+     int c;
+     c=a/b;
+     System.out.println(c);  
     }  
 }  
